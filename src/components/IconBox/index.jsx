975131a06dcd2b3d0a8014be09bfb5382e09a3b7
harshitchugh001 <harshitchugh001@gmail.com>
2024-08-14 02:42:30 +0530

    openings

--- src/components/IconBox/index.jsx
+++ src/components/IconBox/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 import parse from 'html-react-parser';
-import './iconbox.scss'
+import './iconbox.scss';
 import Div from '../Div';
 
-export default function IconBox({title, subtitle, icon,require}) {
+export default function IconBox({ title, subtitle, icon, require }) {
+  
+  const mailtoLink = `mailto:your-email@example.com?subject=Applying for ${title}`;
+
   return (
     <Div className="cs-iconbox cs-style1 text-center">
       <Div className="cs-iconbox_icon">
@@ -12,6 +15,11 @@ export default function IconBox({title, subtitle, icon,require}) {
       <h2 className="cs-iconbox_title">{parse(title)}</h2>
       <Div className="cs-iconbox_subtitle">{parse(subtitle)}</Div>
       <p className="cs-iconbox_subtitle">{require}</p>
+      <Div>
+        <a href={mailtoLink} className="cs-apply-button">
+          Apply
+        </a>
+      </Div>
     </Div>
-  )
+  );
 }
