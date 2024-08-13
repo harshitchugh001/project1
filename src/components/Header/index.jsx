f895ba5421a70d4f851c16c4e70aca9e6cb12c4e
harshitchugh001 <harshitchugh001@gmail.com>
2024-08-14 01:06:23 +0530

    update link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -143,7 +143,7 @@ export default function Header({ variant }) {
                       
                     </li>
                     <li className="">
-                      <Link to="blog" onClick={() => setMobileToggle(false)}>
+                      <Link to="/Websiteblog" onClick={() => setMobileToggle(false)}>
                         Blog
                       </Link>
                       {/* <DropDown>
