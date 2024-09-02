155db1d17237c81681bf34e50a6a48226e0dd052
ByteWiizard <ByteWiizard@gmail.com>
2024-09-02 18:50:59 +0530

    Images Added

---
+++ src/components/content/services.js
@@ -0,0 +1,68 @@
+import { LiquidationSvg, InOutTestingSvg, FairnessOpinionSvg, INDAScomplianceSvg, allocationSvg, ImpairmentTestingSvg, ExpertWitnessTestimonySvg, FamilySettlementSvg, LigitationSvg, DissentingShareholderSvg, ClaimsDamageAssessmentSvg, MergersAcquisationSvg, CorporateRestructoringSvg, FundNavComputationSvg, EsopValuationSvg, DebtRestructoringSvg } from "../svgs/SvgData"
+
+export const ServicesData = [
+    {
+        name: 'Liquidation(IBC)',
+        image: LiquidationSvg,
+    },
+    {
+        name: `Inward & Outward Transfers`,
+        image: InOutTestingSvg,
+    },
+    // {
+    //     name: `Fairness Opinions`,
+    //     image: FairnessOpinionSvg,
+    // },
+    // {
+    //     name: 'IND-AS Compliances',
+    //     image: INDAScomplianceSvg,
+    // },
+    // {
+    //     name: 'Purchase Price Allocations',
+    //     image: allocationSvg,
+    // },
+    // {
+    //     name: 'Impairment Testing',
+    //     image: ImpairmentTestingSvg,
+    // },
+    // {
+    //     name: 'Expert Witness Testimony',
+    //     image: ExpertWitnessTestimonySvg,
+    // },
+    // {
+    //     name: 'Family Settlements',
+    //     image: FamilySettlementSvg,
+    // },
+    // {
+    //     name: 'Litigation Support',
+    //     image: LigitationSvg,
+    // },
+    // {
+    //     name: `Dissenting Shareholder's `,
+    //     image: DissentingShareholderSvg,
+    // },
+    // {
+    //     name: 'Claims/Damage assessments',
+    //     image: ClaimsDamageAssessmentSvg,
+    // },
+    // {
+    //     name: 'Mergers & Acquisitions',
+    //     image: MergersAcquisationSvg,
+    // },
+    // {
+    //     name: 'Corporate Restructurings',
+    //     image: CorporateRestructoringSvg,
+    // },
+    // {
+    //     name: 'Fund NAV Computations',
+    //     image: FundNavComputationSvg,
+    // },
+    // {
+    //     name: 'ESOP/Sweat Equity Valuations',
+    //     image: EsopValuationSvg,
+    // },
+    // {
+    //     name: 'Debt Restructurings',
+    //     image: DebtRestructoringSvg,
+    // }
+]
