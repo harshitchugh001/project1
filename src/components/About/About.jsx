3a661d2fb3f42207e4ff8d793e574734c41cde6e
ByteWiizard <ByteWiizard@gmail.com>
2024-09-04 08:02:08 +0530

    About Us changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className="  mt-28 container mx-auto p-6">
       {/* Who We Are Section */}
       <div className="mb-8">
-        <h2 className="text-4xl font-semibold mb-8 animate-fadeIn text-center">Who We Are</h2>
+        <h2 className="text-2xl md:text-4xl font-semibold mb-8 animate-fadeIn text-center">Who We Are</h2>
         <p className="text-lg text-gray-700 animate-fadeIn">
           <strong>Futurity</strong> is a unified team of leading technocommercial experts dedicated to delivering
           simple solutions to complex issues within a seamless and time-bound framework. Operating under the aegis of
@@ -15,48 +15,48 @@ const About = () => {
           leveraging diverse domain expertise, scale, and a deep understanding to offer exceptional and tailored
           services to our clients.
         </p>
-        <h3 className="text-3xl font-semibold mt-10 mb-4 animate-fadeIn text-center">Our Core Values</h3>
+        <h3 className="text-xl md:text-3xl font-semibold mt-10 mb-4 animate-fadeIn text-center">Our Core Values</h3>
         <div>
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
+          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
             <div className='flex flex-col justify-center items-center'>
-              <span className='w-48 h-48'>
+              <span className='w-28 h-28 md:w-48 md:h-48'>
                 {IntegritySvg}
               </span>
 
-              <span className='font-medium text-xl'>
+              <span className='font-medium text-base md:text-xl'>
                 Integrity
               </span>
 
             </div>
 
             <div className='flex flex-col justify-center items-center'>
-              <span className='w-48 h-48'>
+              <span className='w-28 h-28 md:w-48 md:h-48'>
                 {InnovationSvg}
               </span>
 
-              <span className='font-medium text-xl'>
+              <span className='font-medium text-base md:text-xl'>
                 Innovation
               </span>
 
             </div>
 
             <div className='flex flex-col justify-center items-center'>
-              <span className='w-48 h-48'>
+              <span className='w-28 h-28 md:w-48 md:h-48'>
                 {CoherenceSvg}
               </span>
 
-              <span className='font-medium text-xl'>
+              <span className='font-medium text-base md:text-xl'>
                 Coherence
               </span>
 
             </div>
 
             <div className='flex flex-col justify-center items-center gap-y-4'>
-              <span className='w-48 h-48'>
+              <span className='w-28 h-28 md:w-48 md:h-48'>
                 {DiligenceSvg}
               </span>
 
-              <span className='font-medium text-xl'>
+              <span className='font-medium text-base md:text-xl'>
                 Diligence
               </span>
 
