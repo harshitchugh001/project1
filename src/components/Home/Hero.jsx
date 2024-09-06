4f7ef2a40a59581c269c856a39aff3f4f2ae29c5
harshitchugh001 <harshitchugh001@gmail.com>
2024-09-06 17:50:52 +0530

    assets

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Lottie from 'lottie-react';
 // import animationData from '../../assets/Animation - 1724827126639.json';
-import Test from '../../assets/Test.json'
+// import Test from '../../assets/Test.json'
+import Converted from '../../assets/test4.json'
 import GreyButton from '../buttons/GreyButton';
 import { useNavigate } from 'react-router-dom';
 const Hero = () => {
@@ -13,13 +14,13 @@ const Hero = () => {
     return (
         <div className='flex flex-col justify-center text-center items-center h-[400px] md:h-[850px]'>
             <div className="block md:hidden">
-                <Lottie animationData={Test} loop={true} style={{
+                <Lottie animationData={Converted} loop={true} style={{
                     width: 400,
                     height: 400,
                 }} />
             </div>
             <div className="hidden md:block">
-                <Lottie animationData={Test} loop={true} style={{
+                <Lottie animationData={Converted} loop={true} style={{
                     width: 800,
                     height: 800,
 
